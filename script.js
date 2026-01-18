61c095de63ec97d585b187e76cef1d0e9766766b
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-18 14:29:57 -0500

    creation de fonctions et evenements sur les buttons

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ let pResult = document.getElementById("showTimer");
 
 let timer, timeShow;
 
+//Creation  des fonctions pour les fonctionnalite
+function start() {
+	console.log("Bravo start");
+}
+function stop() {
+	console.log("Brovo stop");
+}
+function reset() {
+	console.log("Brovo reset");
+}
 
+//Ajoutons des evenement a nos buttons
+btnStart.addEventListener("click", start);
+btnStop.addEventListener("click", stop);
+btnReset.addEventListener("click", reset);
