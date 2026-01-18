cfb848affd7f7936820dfff93e255748e722a305
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-18 14:18:12 -0500

    debut de js et declaration des variables

---
+++ script.js
@@ -0,0 +1,10 @@
+//Declaration et initialisation de nos variable
+let btnStart = document.getElementById("startBtn");
+let btnStop = document.getElementById("stopBtn");
+let btnReset = document.getElementById("resetBtn");
+
+let pResult = document.getElementById("showTimer");
+
+let timer, timeShow;
+
+
