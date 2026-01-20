557f09ae3e4c1e6a8381143393977d9503780ffc
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-19 22:08:36 -0500

    disabled btn start when click on it done

--- script.js
+++ script.js
@@ -6,18 +6,30 @@ let btnReset = document.getElementById("resetBtn");
 let pResult = document.getElementById("showTimer");
 
 let timer, timeShow;
-
+let btnAll = document.querySelectorAll("button");
 //Creation  des fonctions pour les fonctionnalite
 function start() {
+	btnStart.disabled = true;
+	btnStop.disabled = false;
+	btnReset.disabled = false;
 	console.log("Bravo start");
 }
+
 function stop() {
+	btnStop.disabled = true;
+
+	btnStart.disabled = false;
+	btnReset.disabled = false;
 	console.log("Brovo stop");
 }
+
 function reset() {
 	console.log("Brovo reset");
 }
 
+if (btnAll.disabled) {
+	btnAll.style.backgroundColor = "gray";
+}
 //Ajoutons des evenement a nos buttons
 btnStart.addEventListener("click", start);
 btnStop.addEventListener("click", stop);
